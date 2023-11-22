676a867b565c3a8ca97cba9e878e7b57de11ce96
henriquesd <hsd.foz@gmail.com>
2023-11-22 20:27:27 +0100

    Created Primary Constructors examples

---
+++ DotNet8Examples/PrimaryConstructors.cs
@@ -0,0 +1,79 @@
+﻿namespace DotNet8Examples
+{
+    public static class PrimaryConstructors
+    {
+        #region Class example
+
+        // Primary Constructor example;
+        public class Book(int id, string title, IEnumerable<decimal> ratings)
+        {
+            // It's also possible to have multiple constructors, and for that, it's necessary to use the this(…) keyword (which will call the main/primary constructor);
+            public Book(int id, string title) : this(id, title, Enumerable.Empty<decimal>()) { }
+            
+            public Book() : this(99, "Demo book") { }
+
+            // Id and Title are are read-only, and the values will be received via Primary Constructor;
+            public int Id => id;
+            public string Title => title.Trim();
+
+            // In the Pages property it will be possible to read and write value;
+            public int Pages { get; set; }
+
+            public decimal AverageRating => ratings.Any() ? ratings.Average() : 0m;
+        }
+
+        // Default Constructor example;
+        public class BookDefault
+        {
+            public int Id { get; }
+            public string Title { get; }
+            public int Pages { get; set; }
+            private readonly List<decimal> ratings = new List<decimal>();
+            public decimal? AverageRating => ratings.Any() ? ratings.Average() : 0m;
+
+            public BookDefault(int id, string title, IEnumerable<decimal>? rating = null)
+            {
+                Id = id;
+                Title = title;
+
+                if (rating?.Any() == true)
+                {
+                    ratings.AddRange(rating);
+                }
+            }
+        }
+        #endregion
+
+        #region Dependency Injection example
+        public interface IBookRepository
+        {
+            Task<List<Book>> GetAll();
+        }
+
+        // Dependency Injection with Primary Constructor example;
+        public class BookService(IBookRepository bookRepository)
+        {
+            public async Task<IEnumerable<Book>> GetAll()
+            {
+                return await bookRepository.GetAll();
+            }
+        }
+
+        // Default Dependency Injection example;
+        public class BookServiceDefault
+        {
+            private readonly IBookRepository _bookRepository;
+
+            public BookServiceDefault(IBookRepository bookRepository)
+            {
+                _bookRepository = bookRepository;
+            }
+
+            public async Task<IEnumerable<Book>> GetAll()
+            {
+                return await _bookRepository.GetAll();
+            }
+        }
+        #endregion
+    }
+}
--- DotNet8Examples/Program.cs
+++ DotNet8Examples/Program.cs
@@ -1,11 +1,35 @@
 ﻿using DotNet8;
+using System.Text.Json;
+using static DotNet8Examples.PrimaryConstructors;
 using MyConsole = System.Console;
 
-MyConsole.WriteLine("Hello .NET 8!");
+Console.WriteLine("Hello .NET 8!");
+
+#region Alias any type
 
 MyConsole.WriteLine("Test console");
 
 var person = AliasAnyType.GetPerson();
-MyConsole.WriteLine(person);
+Console.WriteLine(person);
 
 AliasAnyType.PrintPerson(person);
+
+#endregion
+
+#region Primary Constructors
+
+var book1 = new BookDefault(1, "The Lord of The Rings the Fellowship of the Ring", new List<decimal>() { 5, 4, 4, 5 });
+Console.WriteLine($"{nameof(book1)}: {JsonSerializer.Serialize(book1)}");
+
+var book2 = new Book(2, "The Lord of The Rings the Two Towers", new List<decimal>() { 4, 3, 2, 4 });
+book2.Pages = 352;
+//book2.Title = "The Lord of The Rings the Return of the King";
+Console.WriteLine($"{nameof(book2)}: {JsonSerializer.Serialize(book2)}");
+
+var book3 = new Book(3, "The Lord of the Rings the Return of the King");
+Console.WriteLine($"{nameof(book3)}: {JsonSerializer.Serialize(book3)}");
+
+var book4 = new Book();
+Console.WriteLine($"{nameof(book4)}: {JsonSerializer.Serialize(book4)}");
+
+#endregion

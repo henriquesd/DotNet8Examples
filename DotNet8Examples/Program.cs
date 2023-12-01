192a8a16ec572161bc2184fe20afa299946ed70d
henriquesd <hsd.foz@gmail.com>
2023-12-01 18:42:33 +0100

    Updated Program.cs

--- DotNet8Examples/Program.cs
+++ DotNet8Examples/Program.cs
@@ -31,6 +31,7 @@ Console.WriteLine($"{nameof(book1)}: {JsonSerializer.Serialize(book1)}");
 
 var book2 = new Book(2, "The Lord of The Rings the Two Towers", new List<decimal>() { 4, 3, 2, 4 });
 book2.Pages = 352;
+// The line below will make the compiler issue an error;
 //book2.Title = "The Lord of The Rings the Return of the King";
 Console.WriteLine($"{nameof(book2)}: {JsonSerializer.Serialize(book2)}");
 
@@ -74,7 +75,7 @@ Console.WriteLine($"Experimental Attribute examples:{Environment.NewLine}");
 var demo = new ExperimentalAttributeDemo();
 #pragma warning restore Test001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-// It's configured on the .csproj property to ignore the Test002;
+// It's configured on .csproj to ignore the Test002;
 demo.Print();
 
 Console.WriteLine("-------------------------");
@@ -94,7 +95,7 @@ Console.WriteLine($"Your number is now {numberExample1}");
 
 Console.WriteLine("Method with in:");
 var numberExample2 = 2;
-refOnlyParametersDemo.PrintWithIn(ref numberExample2);
+refOnlyParametersDemo.PrintWithIn(numberExample2);
 Console.WriteLine($"Your number is now {numberExample2}");
 
 Console.WriteLine("Method with ref:");
